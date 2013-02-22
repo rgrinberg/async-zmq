50ee0fe02e7c6d19865cfed712144e12621eeb1a
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-02-21 21:04:23 -0500

    added rep/rep sample

---
+++ samples/rep.ml
@@ -0,0 +1,21 @@
+open Core.Std
+open Async.Std
+
+module AZMQ = Async_zmq.Socket
+
+let rep () =
+  let z = ZMQ.init () in
+  let socket = ZMQ.Socket.create z ZMQ.Socket.rep in
+  ZMQ.Socket.bind socket "tcp://127.0.0.1:5555";
+  print_endline "created zmq socket...";
+  let sock = AZMQ.of_socket socket in
+  print_endline "Waiting for a message";
+  upon (AZMQ.recv sock >>= (fun msg ->
+    Printf.printf "Received: '%s', sending a reply" msg;
+    AZMQ.send sock "reply"
+  )) (fun () ->
+    ZMQ.Socket.close socket;
+    ZMQ.term z; Shutdown.shutdown 0; ()
+  )
+
+let () = rep (); never_returns (Scheduler.go ())
---
+++ samples/req.ml
@@ -0,0 +1,25 @@
+open Core.Std
+open Async.Std
+
+module AZMQ = Async_zmq.Socket
+
+let req z socket =
+  print_endline "created zmq socket...";
+  (*AZMQ.of_socket_async socket >>> (fun sock ->*)
+  let sock = AZMQ.of_socket socket in
+  print_endline "Sending a request";
+  upon (AZMQ.send sock "REQUEST: req.ml" >>= (fun _ ->
+    print_endline "Request sent. Waiting for a reply";
+    AZMQ.recv sock >>= (fun reply ->
+      Printf.printf "Received: '%s'" reply;
+      return ()
+    ))) (fun _ ->
+    ZMQ.Socket.close socket;
+    ZMQ.term z; Shutdown.shutdown 0; ())
+
+let () = 
+  let z = ZMQ.init () in
+  let socket = ZMQ.Socket.create z ZMQ.Socket.req in
+  ZMQ.Socket.connect socket "tcp://127.0.0.1:5555";
+  req z socket; never_returns (Scheduler.go ())
+
