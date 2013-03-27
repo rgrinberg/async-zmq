c5f056d3a9f6ac48252890bc7acf876f916bb353
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-03-26 21:17:54 -0400

    use extract_exn on monitor try_with now

--- lib/async_zmq.ml
+++ lib/async_zmq.ml
@@ -1,13 +1,14 @@
-module Socket = struct
-  open Core.Std
-  open Async.Std
-
+open Async.Std
+open Core.Std
+module Raw = struct
   exception Break_event_loop
   exception Retry
 
   type 'a t = {
     socket : 'a ZMQ.Socket.t;
     fd : Async.Std.Fd.t; }
+
+  let try_extract = Monitor.try_with ~extract_exn:true
 
   let of_socket socket = 
     let fd = ZMQ.Socket.get_fd socket in
@@ -16,8 +17,8 @@ module Socket = struct
   let zmq_event socket ~f =
     let open ZMQ.Socket in
     try begin match events socket with
-              | No_event -> raise Retry
-              | Poll_in | Poll_out | Poll_in_out -> f socket end
+      | No_event -> raise Retry
+      | Poll_in | Poll_out | Poll_in_out -> f socket end
     with | ZMQ.ZMQ_exception (ZMQ.EAGAIN, _) -> raise Retry
          | ZMQ.ZMQ_exception (ZMQ.EINTR, _) -> raise Break_event_loop
 
@@ -29,23 +30,23 @@ module Socket = struct
     in
     let rec idle_loop () =
       let open ZMQ in
-      Monitor.try_with ~name:"<idle_loop>" (fun () -> f socket) >>= function
-        | Ok x -> return x
-        | Error _exn -> begin
-            match (Monitor.extract_exn _exn) with
-            | ZMQ_exception (EINTR, _) -> idle_loop ()
-            | ZMQ_exception (EAGAIN, _) -> begin
-              let rec inner_loop () =
-                Monitor.try_with ~name:"<io_loop>" io_loop >>= function
-                  | Ok x -> x
-                  | Error _exn -> begin 
-                    match (Monitor.extract_exn _exn) with
-                    | Break_event_loop -> idle_loop ()
-                    | Retry -> inner_loop ()
-                    end
-              in inner_loop ()
+      Monitor.try_with ~extract_exn:true ~name:"<idle_loop>"
+        (fun () -> f socket) >>= function
+      | Ok x -> return x
+      | Error (ZMQ_exception (EINTR, _)) -> idle_loop ()
+      | Error (ZMQ_exception (EAGAIN, _)) -> begin
+          let rec inner_loop () =
+            Monitor.try_with ~extract_exn:true ~name:"<io_loop>" io_loop >>= function
+            | Ok x -> x
+            | Error _exn -> begin 
+                match _exn with
+                | Break_event_loop -> idle_loop ()
+                | Retry -> inner_loop ()
+                | x -> raise x (* is this necessary *)
               end
-            end
+          in inner_loop ()
+        end
+      | Error x -> raise x (* is this necessary? *)
     in idle_loop ()
 
   let recv s =
@@ -54,4 +55,3 @@ module Socket = struct
   let send s m =
     wrap (fun s -> ZMQ.Socket.send ~opt:ZMQ.Socket.S_no_block s m) s
 end
-
